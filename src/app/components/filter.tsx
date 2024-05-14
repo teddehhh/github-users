93c228af6ed37ca4c1f76f832ec6f42ab19159f4
Egor Vorobev <vorobev.roge@gmail.com>
2024-05-14 11:40:56 +0500

    feat: Добавлено открытие фильтров по умолчанию

--- src/app/components/filter.tsx
+++ src/app/components/filter.tsx
@@ -19,7 +19,11 @@ const Filter: FunctionComponent<FilterProps> = (props) => {
   return (
     <div className="flex flex-col justify-between w-1/4 m-2 border-2 px-2 py-1 text-sm overflow-y-auto">
       <div>
-        <Accordion type="multiple" className="w-full">
+        <Accordion
+          defaultValue={['item-1', 'item-2']}
+          type="multiple"
+          className="w-full"
+        >
           <AccordionItem value="item-1">
             <AccordionTrigger>Login</AccordionTrigger>
             <AccordionContent>
@@ -28,7 +32,7 @@ const Filter: FunctionComponent<FilterProps> = (props) => {
           </AccordionItem>
           <AccordionItem value="item-2">
             <AccordionTrigger>User Type</AccordionTrigger>
-            <AccordionContent>
+            <AccordionContent aria-expanded>
               <div className="flex items-center space-x-2">
                 <Checkbox id="terms" checked disabled />
                 <label
